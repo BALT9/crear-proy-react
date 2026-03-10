13f31e31387b0a9142fa604d549e79ee9c5c23a8
willy <willybalt.2017@gmail.com>
2026-03-09 23:52:26 -0400

    creacion de proyecto react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Saludo from "./Saludo"
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-red-500 h-40 m-50">
+        Hola Brayan
+      </p>
+      <Saludo />
+      <Saludo > </Saludo>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Saludo.jsx
@@ -0,0 +1,10 @@
+function Saludo() {
+    return (
+        <div>
+            <h1>Hola Soy Brayan desde componente saludo</h1>
+            <h2>Saludando...</h2>
+        </div>
+    )
+}
+
+export default Saludo
